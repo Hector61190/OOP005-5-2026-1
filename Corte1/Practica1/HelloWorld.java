3b76f81bc5b145cf1b0e3fe5691a85f5224942e5
Hector61190 <agudelochacua17@gmail.com>
2026-02-09 13:46:10 -0500

    Add files via upload

---
+++ Corte1/Practica1/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
