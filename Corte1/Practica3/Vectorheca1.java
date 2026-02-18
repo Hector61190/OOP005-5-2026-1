31b32272b5aabea46dddb928a93bf91bb83c2cf5
Hector61190 <agudelochacua17@gmail.com>
2026-02-17 22:36:31 -0500

    Add files via upload

---
+++ Corte1/Practica3/Vectorheca1.java
@@ -0,0 +1,44 @@
+package vectorheca1;
+
+import java.util.Scanner;
+
+public class Vectorheca1 {
+
+    public int[] data;
+    public int n;
+
+    // Constructor: crea vector de tamaño n
+    public Vectorheca1(int n) {
+        this.n = n;
+        this.data = new int[n];
+    }
+
+    // Método para ingresar valores uno por uno
+    public void setArray() {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingresar el valor para la posicion " + i + ": ");
+            data[i] = sc.nextInt();
+        }
+    }
+
+    // Método para imprimir el vector
+    public void print(String name) {
+        System.out.print(name + " = [ ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(data[i]);
+            if (i < n - 1) System.out.print(", ");
+        }
+
+        System.out.println(" ]");
+    }
+
+    public static void main(String[] args) {
+        Vectorheca1 ob1 = new Vectorheca1(8);
+
+        ob1.setArray();
+        ob1.print("Hola");
+    }
+}
